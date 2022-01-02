5eb8f172cbeef81eba34cb9327721aab18cd62e1
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2022-01-03 00:59:22 +0330

    Create starting.sh

---
+++ starting.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+unzip cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TUB1N4ZUEmeGroemVHuZoz9beJYdnHJ6Dt.test
